cafd3c714b54bc986e2a2cd8ab00666fd55cc3b9
acieply <arturcieplyy@gmail.com>
2017-04-23 09:32:23 +0200

    Dodanie wprawki 3

--- src/main/java/pl/sternik/weekend/web/controlers/WprawkiController.java
+++ src/main/java/pl/sternik/weekend/web/controlers/WprawkiController.java
@@ -6,12 +6,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class WprawkiController {
+public class WprawkiController { 
 
+	
+//	@GetMapping("/wprawki2")
+//	@ResponseBody
+//	public String wprawkiParam(@RequestParam("cos") String cosParam){
+//		return "Wprawki z param cos =" + cosParam)
+//	}
+	
+	
 	@RequestMapping(path = "/wprawki", method = RequestMethod.GET)
 	public String wprawki(ModelMap model) {
 		model.put("msg", "Wartosc z modelu");
@@ -26,5 +36,14 @@ public class WprawkiController {
 		model.addAttribute("data", new Date());
 		return "wprawki";
 	}
+	
+	@GetMapping("/wprawki3")
+    @ResponseBody
+    public String wprawkiHeader(@RequestHeader("User-Agent") String cosParam) {
+    	return "Uzywasz przegladarki:=" + cosParam;
+    }
+	
+	
+	
 
 }
