d049bf94d436c27ffbd0f914b14272d72cdcc581
acieply <arturcieplyy@gmail.com>
2017-04-23 09:01:38 +0200

    Poprawki do wprawkiController

--- src/main/java/pl/sternik/weekend/web/controlers/WprawkiController.java
+++ src/main/java/pl/sternik/weekend/web/controlers/WprawkiController.java
@@ -4,25 +4,27 @@ import java.util.Date;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class WprawkiController {
 
-    @RequestMapping("/wprawki")
-    public String wprawki(ModelMap model) {
-        model.put("msg", "Wartosc z modelu");
-        model.addAttribute("data", new Date());
-        return "wprawki";
-    }
+	@RequestMapping(path = "/wprawki", method = RequestMethod.GET)
+	public String wprawki(ModelMap model) {
+		model.put("msg", "Wartosc z modelu");
+		model.addAttribute("data", new Date());
+		return "wprawki";
+	}
 
-    @RequestMapping("/wprawki/{cos}")
-    public String wprawki(@PathVariable String cos, ModelMap model) {
-        model.addAttribute("cos", cos);
-        model.put("msg", "Wartosc z modelu");
-        model.addAttribute("data", new Date());
-        return "wprawki";
-    }
+	@GetMapping("/wprawki/{cos}")
+	public String wprawki(@PathVariable String cos, ModelMap model) {
+		model.addAttribute("cos", cos);
+		model.put("msg", "Wartosc z modelu");
+		model.addAttribute("data", new Date());
+		return "wprawki";
+	}
 
 }
