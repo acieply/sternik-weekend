2fd0d9b2a5ab8b15003f4e9b0501463f6215b33e
acieply <arturcieplyy@gmail.com>
2017-05-14 09:01:39 +0200

    @Autowired and change port

--- src/main/java/pl/sternik/weekend/web/controlers/KlaserController.java
+++ src/main/java/pl/sternik/weekend/web/controlers/KlaserController.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -20,6 +21,7 @@ public class KlaserController {
 
 	@Autowired
 	//@Qualifier("spring")
+	@Qualifier("tablica")
 	private KlaserService klaserService;
 
 	@Autowired
